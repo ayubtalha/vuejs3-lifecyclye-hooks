a57bbbac857c0f1f468a959532062b2484fb0ff7
Talha-Ayub-github <mtalhaayub752@gmail.com>
2021-08-18 13:26:07 +0500

    VUE JS 3 - Youtube Ninja. Reaction Timer Project with LifeCycle Hooks and Child to Parent Data Passing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Recation Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <!-- @end (CHILD TO PARENT DATA PASSING) -->
+  <Block v-if="isPlaying" v-bind:delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  components: { Block, Results },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+      console.log("delay:", this.delay);
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +43,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>Reaction Time Score is - {{ score }} ms</p>
+    <p class="rank">{{ rank }} Speed</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 500) {
+      this.rank = "Impressive";
+    } else if (this.score < 1000) {
+      this.rank = "Good";
+    } else {
+      this.rank = "Too Slow ...";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
